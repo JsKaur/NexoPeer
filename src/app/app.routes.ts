06ef50e26a8b7272b6b350e9354186da4bd74a0c
Jashanpreet Kaur Chahal <chahal63@sheridancollege.ca>
2025-01-18 13:20:16 -0500

    fixed issue of spline

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
 import { FooterComponent } from '../components/footer/footer.component';
+import { PostComponent } from '../components/post/post.component';
+import { SignINComponent } from '../components/sign-in/sign-in.component';
 export const routes: Routes = [
     { path: '', component: DashboardComponent },
-    {path:'footer',component:FooterComponent}
+    {path:'footer',component:FooterComponent},
+    {path:'post',component:PostComponent},
+    {path:'sign',component:SignINComponent}
+    
 ];
