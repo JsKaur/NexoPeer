949a40efe1f7df24a73479cf5e0ccd221905e713
Alshifa Belim <belimal@sheridancollege.ca>
2025-01-18 13:06:48 -0500

    Updating Dashboard UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomepageComponent } from "./homepage/homepage.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { DashboardComponent } from "../components/dashboard/dashboard.component";
+import { FooterComponent } from "../components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomepageComponent, NavbarComponent, DashboardComponent],
+  imports: [RouterOutlet, HomepageComponent, NavbarComponent, DashboardComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
